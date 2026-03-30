afb738ae98b4e5996c923d6b909815024a9fec74
Zahiruddin <97023186+muhdZahir@users.noreply.github.com>
2026-03-30 20:32:03 +0800

    Add files via upload

---
+++ NetBeans/GUIAssignment/src/guiassignment/GUIAssignment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guiassignment;
+
+/**
+ *
+ * @author Othman Family
+ */
+public class GUIAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
